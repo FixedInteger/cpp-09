6a1b72b6b6970dc5f8cef7448bbd25ceba29915c
hossine eddahbi <eddahbi.hossine.solicode@gmail.com>
2024-03-07 18:52:03 +0100

    add recursion sort [bro work]

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -67,6 +67,19 @@ std::list<std::pair<int, int> > pmerge(const std::list<int>& numbers)
     return res;
 }
 
+void recursion_sort(std::list<std::pair<int,int> > o)
+{
+    std::list<std::pair<int,int> >::iterator it;
+    for(it = o.begin(); it != o.end(); it++)
+    {
+        if(it->first > std::next(it)->first)
+        {
+            std::pair<int,int> temp = *it;
+            *it = *std::next(it);
+            *std::next(it) = temp;
+        }
+    }
+}
 int main(int argc, char *argv[]) {
     try {
         if (argc <= 1) 
@@ -81,10 +94,11 @@ int main(int argc, char *argv[]) {
 			std::cout<<"strugller "<<strugller<<std::endl;
 		}
         std::list<std::pair<int, int> > pairs = pmerge(numbers);
+        recursion_sort(pairs);
         std::list<std::pair<int, int> >::iterator it3;
 		for(it3 = pairs.begin() ; it3 != pairs.end(); it3++)
         {
-            std::cout << it3->first << " | " << it3->second << std::endl;
+            std::cout << it3->first << " " << it3->second << std::endl;
         }
     }
     catch (std::exception &e) 
